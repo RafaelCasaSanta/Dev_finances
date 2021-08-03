a5eddfb7fadbce1f2b3b237c7589d1d1ae969198
Rafael Casa <rafael.casasanta@edu.unifil.br>
2021-08-03 13:47:44 -0300

    Working code

--- scripts.js
+++ scripts.js
@@ -1,85 +1,63 @@
-//Tema
-let darkMode = localStorage.getItem("darkMode");
-
-
-
-
-//!Construção do Modal
-const Modal = {
-    open() {
+var darkMode = localStorage.getItem("darkMode");
+var Modal = {
+    open: function () {
         document.querySelector(".modal-overlay").classList.add("active");
     },
-    close() {
+    close: function () {
         document.querySelector(".modal-overlay").classList.remove("active");
-    },
-};
-
-//!Armazenamento dos itens 
-const Storage = {
-    get() {
-        return JSON.parse(localStorage.getItem("dev.finances:transactions")) ||
-            []
-    },
-
-    set(transactions) {
-        localStorage.setItem(
-            "dev.finances:transactions", JSON.stringify(transactions))
     }
 };
-
-//!Dark Theme 
-const Theme = {
-    enable() {
+var LocalStorage = {
+    get: function () {
+        return JSON.parse(localStorage.getItem("dev.finances:transactions")) ||
+            [];
+    },
+    set: function (transactions) {
+        localStorage.setItem("dev.finances:transactions", JSON.stringify(transactions));
+        console.log("estou aqui");
+    }
+};
+var Theme = {
+    enable: function () {
         document.body.classList.add("darkmode");
         localStorage.setItem("darkMode", "enabled");
         document.getElementById("theme_icon").src = "./assets/theme-dark.svg";
         //document.querySelector(".background").src = "./assets/background-shape-dark.svg";
         //ocument.querySelector(".svg-down").src = "./assets/down-dark.svg";
     },
-
-    disable() {
+    disable: function () {
         document.body.classList.remove("darkmode");
         localStorage.setItem("darkMode", "null");
         document.getElementById("theme_icon").src = "./assets/theme-light.svg";
         //document.querySelector(".svg-down").src = "./assets/down";
     },
-
-    click() {
-        let darkMode = localStorage.getItem("darkMode");
-
+    click: function () {
+        var darkMode = localStorage.getItem("darkMode");
         if (darkMode !== "enabled") {
             this.enable();
-        } else {
+        }
+        else {
             this.disable();
         }
-    },
+    }
 };
-
-//!Regras de negócio do Transaction
-const Transaction = {
+var Transaction = {
     //Criando um atalho
-    all: Storage.get(),
-
+    all: LocalStorage.get(),
     //Adicionar uma transação a tela
-    add(transaction) {
+    add: function (transaction) {
         Transaction.all.push(transaction);
-
         App.reload();
     },
-
-    remove(index) {
-        
-        Transaction.all.splice(index, 1)
-
+    remove: function (index) {
+        Transaction.all.splice(index, 1);
         App.reload();
     },
-
-    incomes() {
-        let income = 0;
-
+    incomes: function () {
+        var income = 0;
         //pegar todas as transações
         //para cada  transação,
-        Transaction.all.forEach((transaction) => {
+        Transaction.all.forEach(function (transaction) {
             //se maior que zero
             if (transaction.amount > 0) {
                 //somar a uma variavel e retornar variavel
@@ -88,218 +66,155 @@ const Transaction = {
         });
         return income;
     },
-
-    expenses() {
-        let expense = 0;
-        Transaction.all.forEach((transaction) => {
+    expenses: function () {
+        var expense = 0;
+        Transaction.all.forEach(function (transaction) {
             if (transaction.amount < 0) {
                 expense += transaction.amount;
             }
         });
         return expense;
     },
-
-    total() {
+    total: function () {
         //soma da entrada - a saida
         return Transaction.incomes() + Transaction.expenses();
-    },
+    }
 };
-
-//!Substituir os dados do html,
-const DOM = {
+var DOM = {
     transactionsContainer: document.querySelector(".data-table tbody"),
-
-    addTransaction(transaction, index) {
-        const tr = document.createElement("tr");
+    addTransaction: function (transaction, index) {
+        var tr = document.createElement("tr");
         tr.innerHTML = DOM.innerHTMLTransaction(transaction, index);
         tr.dataset.index = index;
-
         DOM.transactionsContainer.appendChild(tr);
     },
-
-    innerHTMLTransaction(transaction, index) {
+    innerHTMLTransaction: function (transaction, index) {
         //A cada transação que receber ele vai verificar se é verdadeiro ou falso
-        const CSSclass = transaction.amount > 0 ? "income" : "expense";
-
-        const amount = Utils.formatCurrency(transaction.amount);
-
-        const html = `
-
-        <td class="description">${transaction.description}</td>
-        <td class="quantity">${transaction.quantity}</td>
-        <td class="${CSSclass}">${amount}</td>
-        <td class="date">${transaction.date}</td>
-        <td>
-            <button class="exclude-transation-item">
-                <img onclick="Transaction.remove(${index})" src="./assets/minus.svg" alt="Remover transação" />
-            </button>
-        </td>
-    
-    `;
+        var CSSclass = transaction.amount > 0 ? "income" : "expense";
+        var amount = Utils.formatCurrency(transaction.amount);
+        var html = "\n\n        <td class=\"description\">" + transaction.description + "</td>\n        <td class=\"quantity\">" + transaction.quantity + "</td>\n        <td class=\"" + CSSclass + "\">" + amount + "</td>\n        <td class=\"date\">" + transaction.date + "</td>\n        <td>\n            <button class=\"exclude-transation-item\">\n                <img onclick=\"Transaction.remove(" + index + ")\" src=\"./assets/minus.svg\" alt=\"Remover transa\u00E7\u00E3o\" />\n            </button>\n        </td>\n    \n    ";
         return html;
     },
     //Trabalha com a funcionalidade de uplodar os valores na tela
     //FORMATAÇÃO NA TELA WORKING
-    updateBalance() {
-        document.getElementById("incomeDisplay").innerHTML = Utils.formatCurrency(
-            Transaction.incomes()
-        );
-        document.getElementById("expenseDisplay").innerHTML = Utils.formatCurrency(
-            Transaction.expenses()
-        );
-        document.getElementById("totalDisplay").innerHTML = Utils.formatCurrency(
-            Transaction.total()
-        );
+    updateBalance: function () {
+        document.getElementById("incomeDisplay").innerHTML = Utils.formatCurrency(Transaction.incomes());
+        document.getElementById("expenseDisplay").innerHTML = Utils.formatCurrency(Transaction.expenses());
+        document.getElementById("totalDisplay").innerHTML = Utils.formatCurrency(Transaction.total());
     },
-
-    clearTransactions() {
+    clearTransactions: function () {
         DOM.transactionsContainer.innerHTML = "";
-    },
+    }
 };
-
-//!Formatação dos conteudos.
-const Utils = {
-    formatAmount(value) {
+var Utils = {
+    formatAmount: function (value) {
         value = value * 100;
         return Math.round(value);
     },
-    formatDate(date) {
-        const splittedDate = date.split("-");
-        return ` ${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`;
+    formatDate: function (date) {
+        var splittedDate = date.split("-");
+        return " " + splittedDate[2] + "/" + splittedDate[1] + "/" + splittedDate[0];
     },
-
-    formatCurrency(value) {
-        const signal = Number(value) < 0 ? "-" : "";
-
+    formatCurrency: function (value) {
+        var signal = Number(value) < 0 ? "-" : "";
         value = String(value).replace(/\D/g, "");
-
         value = Number(value) / 100;
-
         value = value.toLocaleString("pt-BR", {
             style: "currency",
-            currency: "BRL",
+            currency: "BRL"
         });
         return signal + value;
-    },
+    }
 };
-
-//!Todas as funcionalidades do Formulario para captura dos dados.
-const Form = {
+var Form = {
     description: document.querySelector("input#description"),
     quantity: document.querySelector("input#quantity"),
     amount: document.querySelector("input#amount"),
     date: document.querySelector("input#date"),
-
-    getValues() {
+    getValues: function () {
         return {
             description: Form.description.value,
             quantity: Form.quantity.value,
             amount: Form.amount.value,
-            date: Form.date.value,
+            date: Form.date.value
         };
     },
-
-    validateFields() {
-        const { description, amount, date, quantity } = Form.getValues();
-
-        if (
-            description.trim() === "" ||
-            quantity.trim() ==="" || 
+    validateFields: function () {
+        var _a = Form.getValues(), description = _a.description, amount = _a.amount, date = _a.date, quantity = _a.quantity;
+        if (description.trim() === "" ||
+            quantity.trim() === "" ||
             amount.trim() === "" ||
-            date.trim() === "" 
-        ) {
+            date.trim() === "") {
             throw new Error("Por favor, preencha todos os campos");
         }
     },
-    formatValues() {
-        let { description, amount, date, quantity } = Form.getValues();
-
+    formatValues: function () {
+        var _a = Form.getValues(), description = _a.description, amount = _a.amount, date = _a.date, quantity = _a.quantity;
         amount = Utils.formatAmount(amount);
-
         date = Utils.formatDate(date);
-        
-        
-
         return {
-            description,
-            quantity,
-            amount,
-            date,
+            description: description,
+            quantity: quantity,
+            amount: amount,
+            date: date
         };
     },
-
-
-
-
-    clearFields() {
+    clearFields: function () {
         Form.description.value = "";
         Form.amount.value = "";
         Form.date.value = "";
     },
-
-    submit(event) {
+    submit: function (event) {
         event.preventDefault();
-
         try {
             //Verificar se todas as informações foram preenchidas
             Form.validateFields();
             //Formatar os dados para salvar.
-            const transaction = Form.formatValues();
+            var transaction = Form.formatValues();
             //Salvar
-            Transaction.add(transaction)
+            Transaction.add(transaction);
             //Apagar dados do formulario
             Form.clearFields();
             //Modal feche
             Modal.close();
-        } catch (error) {
+        }
+        catch (error) {
             alert(error.message);
         }
-    },
+    }
 };
-
-//!Execução do programa
-const App = {
-    init() {
+var App = {
+    init: function () {
         //Para cada transação que tem dentro das transações, roda uma funcionalidade.
         Transaction.all.forEach(DOM.addTransaction);
         //Atualizando os cartões
         DOM.updateBalance();
         //Atualizando o local storage
-        Storage.set(Transaction.all)
-
-
+        LocalStorage.set(Transaction.all);
         //Theme
         if (darkMode === "enabled") {
             Theme.enable();
             document.getElementById("theme_icon").src = "./assets/theme-dark.svg";
         }
-
-
     },
-    reload() {
+    reload: function () {
         DOM.clearTransactions();
         App.init();
-    },
+    }
 };
-
-const Csv = {
-
-    exportCSVFile(headers, items, fileTitle) {
+var Csv = {
+    exportCSVFile: function (headers, items, fileTitle) {
         if (headers) {
             items.unshift(headers);
         }
-
         var jsonObject = JSON.stringify(items);
-
         var csv = Csv.convertToCSV(jsonObject);
-
         var exportedFilename = fileTitle + ".csv" || "export.csv";
-
-
         var blob = new Blob([csv], { type: "text/csv;charset=utf-8" });
         if (navigator.msSaveBlob) {
             navigator.msSaveBlob(blob, exportedFilename);
-        } else {
+        }
+        else {
             var link = document.createElement("a");
             if (link.download !== undefined) {
                 var url = URL.createObjectURL(blob);
@@ -312,55 +227,39 @@ const Csv = {
             }
         }
     },
-    convertToCSV(objArray) {
+    convertToCSV: function (objArray) {
         var array = typeof objArray != "object" ? JSON.parse(objArray) : objArray;
         var str = "";
-
-        for(var i = 0; i < array.length; i++) {
+        for (var i = 0; i < array.length; i++) {
             var line = "";
-
-            for(var index in array[i]){
-                if(line != "") line += ";";
-
+            for (var index in array[i]) {
+                if (line != "")
+                    line += ";";
                 line += array[i][index];
-            
             }
             str += line + "\r\n";
-
         }
-
         return str;
     },
-
-    download() {
+    download: function () {
         var headers = {
             description: "Nome".replace(/,/g, ""),
             quantity: "Quantidade",
             amount: "Valor",
-            date: "Data",
+            date: "Data"
         };
-
-        itemsNotFormatted = Storage.get();
-
+        var itemsNotFormatted = LocalStorage.get();
         var itemsFormatted = [];
-
-
-        itemsNotFormatted.forEach((item) => {
-        itemsFormatted.push({
-            description: item.description,
-            quantity: item.quantity,
-            amount: `R$ ${item.amount} `,
-            date: item.date,
-
+        itemsNotFormatted.forEach(function (item) {
+            itemsFormatted.push({
+                description: item.description,
+                quantity: item.quantity,
+                amount: "R$ " + item.amount + " ",
+                date: item.date
+            });
         });
-
-
-        });
-    var fileTitle = "Minhas Transações - DevFinances";
-
-    Csv.exportCSVFile(headers, itemsFormatted, fileTitle);
-
-    },
+        var fileTitle = "Minhas Transações - DevFinances";
+        Csv.exportCSVFile(headers, itemsFormatted, fileTitle);
+    }
 };
-
 App.init();
